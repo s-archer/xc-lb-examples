91be390574cee3b62f0a2a9d39a1f147c66b33b9
Stephen Archer <s.archer@f5.com>
2024-12-20 13:38:36 +0000

    updated no-code example

--- modules/no-code/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "volt_api_url" {}
-variable "volt_api_p12_file" {}
-variable "namespace" {}
-variable "lb_name" {}
-variable "lb_domains" {}
-# variable "lb_site" {}
-variable "lb_virtual_site" {}
-variable "origin_service_name" {}
-variable "origin_site" {}
